f79abe89bb44a1c479919e3b161635130dca13dd
mmehelay <mmehelay@outlook.com>
2024-09-29 16:50:18 -0400

    full website

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,16 +1,14 @@
 module.exports = {
     "name": "Triple Kay Demolition",
-    "email": "help@TripleKayDemolition.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "email": "info@triplekaydemolition.ca",
+    "phoneForTel": "647-809-9056",
+    "phoneFormatted": "(647) 809-9056",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
-        "country": "US",
-        "mapLink": "https://goo.gl/maps/UAQn4vuGDiwv7DV39"
+        "lineOne": "Scarborough",
+        "city": "Toronto",
+        "state": "On",
+        "country": "CA",
+        "mapLink": "https://www.google.ca/maps/place/Scarborough,+ON/data=!4m2!3m1!1s0x89d4d05e9e66b609:0xf06e63ce2447807b?sa=X&ved=1t:242&ictx=111"
     },
     "socials": {
         "facebook": "https://www.facebook.com/",
